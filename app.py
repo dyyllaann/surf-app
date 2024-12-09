9409d88e034cccf5dbf48f2e58f7e17f90015d45
dyyllaann <dylan.arveson@gmail.com>
2024-12-08 22:58:46 -0800

    Cleaned up app.py to removed code that was moved to notifier.py module.

--- app.py
+++ app.py
@@ -6,15 +6,6 @@ import schedule
 import requests
 from notifier import send
 
-# # Declare text carriers
-# carriers = {
-# 	'att':    '@txt.att.net',
-# 	'tmobile': ' @tmomail.net',
-# 	'verizon':  '@vtext.com',
-# 	'sprint':   '@page.nextel.com',
-#     'googlefi': '@msg.fi.google.com'
-# }
-
 class Buoy:
     swellData = []
     windWaveData = []
@@ -26,22 +17,6 @@ def getLatestDataFromRequests():
     r = requests.get(url)
     data = r.text
     return data
-
-# # Create message function
-# def send(message):
-#     # Send message to to_number from gmail address
-#     to_number = f"3603019197{carriers['googlefi']}"
-
-#     # Gmail account and password
-#     auth = ('straitsurf@gmail.com', 'ubxdboegmwesflzg')
-
-# 	# Establish a secure session with gmail's outgoing SMTP server using your gmail account
-#     server = smtplib.SMTP("smtp.gmail.com", 587)
-#     server.starttls()
-#     server.login(auth[0], auth[1])
-
-# 	# Send text message through SMS gateway of destination number
-#     server.sendmail(auth[0], to_number, message)
 
 def populateBuoy():
     data = getLatestDataFromRequests()
