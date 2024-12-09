b08c7187c1b1b787534df8a109d693f07b614f70
dyyllaann <dylan.arveson@gmail.com>
2024-12-08 22:54:57 -0800

    Extracted the notifier function into a separate module to improve code modularity and maintainability.

--- app.py
+++ app.py
@@ -2,17 +2,18 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import time
 import schedule
-import smtplib
+# import smtplib
 import requests
+from notifier import send
 
-# Declare text carriers
-carriers = {
-	'att':    '@txt.att.net',
-	'tmobile': ' @tmomail.net',
-	'verizon':  '@vtext.com',
-	'sprint':   '@page.nextel.com',
-    'googlefi': '@msg.fi.google.com'
-}
+# # Declare text carriers
+# carriers = {
+# 	'att':    '@txt.att.net',
+# 	'tmobile': ' @tmomail.net',
+# 	'verizon':  '@vtext.com',
+# 	'sprint':   '@page.nextel.com',
+#     'googlefi': '@msg.fi.google.com'
+# }
 
 class Buoy:
     swellData = []
@@ -26,21 +27,21 @@ def getLatestDataFromRequests():
     data = r.text
     return data
 
-# Create message function
-def send(message):
-    # Send message to to_number from gmail address
-    to_number = f"3603019197{carriers['googlefi']}"
+# # Create message function
+# def send(message):
+#     # Send message to to_number from gmail address
+#     to_number = f"3603019197{carriers['googlefi']}"
 
-    # Gmail account and password
-    auth = ('straitsurf@gmail.com', 'ubxdboegmwesflzg')
+#     # Gmail account and password
+#     auth = ('straitsurf@gmail.com', 'ubxdboegmwesflzg')
 
-	# Establish a secure session with gmail's outgoing SMTP server using your gmail account
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(auth[0], auth[1])
+# 	# Establish a secure session with gmail's outgoing SMTP server using your gmail account
+#     server = smtplib.SMTP("smtp.gmail.com", 587)
+#     server.starttls()
+#     server.login(auth[0], auth[1])
 
-	# Send text message through SMS gateway of destination number
-    server.sendmail(auth[0], to_number, message)
+# 	# Send text message through SMS gateway of destination number
+#     server.sendmail(auth[0], to_number, message)
 
 def populateBuoy():
     data = getLatestDataFromRequests()
